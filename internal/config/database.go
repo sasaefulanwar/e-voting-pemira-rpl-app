7136695c9601c4a60af1460ca3e2c3a4a9ab33a8
Nur Saeful Anwar <saaefulanwar53094@gmail.com>
2026-06-13 12:27:31 +0700

    bismillah #2

--- internal/config/database.go
+++ internal/config/database.go
@@ -9,8 +9,11 @@ import (
 )
 
 func InitDB() *sql.DB {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		panic("DATABASE_URL is empty")
+	}
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
